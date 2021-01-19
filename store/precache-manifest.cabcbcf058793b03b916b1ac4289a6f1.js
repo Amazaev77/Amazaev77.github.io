ad29f13eac9c427ca131dd6495a022c753d12846
Amazaev <askhabamazaev@gmail.com>
2021-01-19 17:20:59 +0300

    init

---
+++ store/precache-manifest.cabcbcf058793b03b916b1ac4289a6f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c076237a7089e4c82e272f2cad4a674",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0d61cde632749072e0dd",
+    "url": "./static/js/2.88a60736.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.88a60736.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7b3dd2e57aa87de9adc",
+    "url": "./static/js/main.95b6fae6.chunk.js"
+  },
+  {
+    "revision": "a126dcc470197a062998",
+    "url": "./static/js/runtime-main.bb193d6c.js"
+  },
+  {
+    "revision": "4e6c5b599de59e4850083a1aeb081a66",
+    "url": "./static/media/full-word.4e6c5b59.svg"
+  },
+  {
+    "revision": "8bf1af37dd02198b256725e83a203795",
+    "url": "./static/media/shopping-cart-solid.8bf1af37.svg"
+  }
+]);
---
+++ toDo/precache-manifest.8ebe55279bbc940ba1872dc451babd95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ba963be6ff469cc2e9e30b750369faf",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d166d0134cd0e72ec5fe",
+    "url": "./static/css/main.bf98679d.chunk.css"
+  },
+  {
+    "revision": "61d8778076cfefdc25d1",
+    "url": "./static/js/2.930e3dac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.930e3dac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d166d0134cd0e72ec5fe",
+    "url": "./static/js/main.bda748a0.chunk.js"
+  },
+  {
+    "revision": "37807aa12135f3f0df06",
+    "url": "./static/js/runtime-main.f6a946e7.js"
+  }
+]);
